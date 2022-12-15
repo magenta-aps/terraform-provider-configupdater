7cf98e05119b69a2854fd3730fdfb5c41ae15870
Carl Bordum Hansen <carl@bordum.dk>
2022-12-15 16:09:55 +0100

    feat: [#53005] initial provider

---
+++ main.tf
@@ -0,0 +1,24 @@
+# This file serves as an example of how to use the provider, as well as being
+# useful for development.
+
+terraform {
+  required_providers {
+    configupdater = {
+      version = "~> 1.0.0"
+      source  = "magenta-aps/configupdater"
+    }
+  }
+}
+
+# You only need the provider config if you want to change to another URL.
+provider "configupdater" {
+  url = "http://localhost:8000/"
+}
+
+resource "configupdater_secret" "my-server-name" {
+  file_path = "mailgun/postmaster__magenta.dk.enc.yaml"
+  secret = {
+    asecret = "hunter2"
+    another = "2hunter"
+  }
+}
